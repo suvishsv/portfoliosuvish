8f4934846a9e7b02fefd5387e4702fa920f91c34
suvishsv <suvishraj187@gmail.com>
2025-06-17 22:31:36 +0530

    insert all my portfolio file

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+function togglemenu() {
+    const menuList = document.getElementById("menuList");
+    menuList.classList.toggle("showMenu");
+  }
+
+
+
+
+// const pagereload=new intersectionObserver((entries)=>{
+//     entries.forEach((entry)=>{
+//         if(entry.isIntersecting){
+//             entry.target.classList.add("show");
+//         }
+//         else{
+//             entry.target.classList.remove("show");
+//         }
+//     });
+// });
+
+
+
+
+
+// const reload=document.querySelector(".contentPageSkills");
+// reload.forEach((el)=> pagereload.observe(el));
